65665f2118fc64f02adf193c09c3d1dc5ed1d5eb
pegielm <pegielm@gmail.com>
2023-06-13 20:46:58 +0200

    zad2

--- lab6/zadanie2.c
+++ lab6/zadanie2.c
@@ -1,60 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
+#define INITIAL_CAPACITY 3
+
+// Struktura przechowująca informacje o początkach i końcach obszarów zawierających liczby
 typedef struct {
-    char *start;
-    char *end;
-} NumberRange;
+    char** lines;
+    int lineCount;
+    int capacity;
+} DataStructure;
 
-bool is_valid_digit(char digit, int base) {
-    if (base <= 10) {
-        return (digit >= '0' && digit < '0' + base);
-    } else {
-        return ((digit >= '0' && digit <= '9') || (tolower(digit) >= 'a' && tolower(digit) < 'a' + base - 10));
+// Funkcja inicjalizująca strukturę danych
+DataStructure* initializeDataStructure() {
+    DataStructure* data = (DataStructure*)malloc(sizeof(DataStructure));
+    data->lines = (char**)malloc(INITIAL_CAPACITY * sizeof(char*));
+    data->lineCount = 0;
+    data->capacity = INITIAL_CAPACITY;
+    return data;
+}
+
+// Funkcja wczytująca linie tekstu z klawiatury
+void readLines(DataStructure* data) {
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        if (data->lineCount >= data->capacity) {
+            // Jeśli liczba linii przekracza pojemność, dokonaj realokacji pamięci
+            int newCapacity = data->capacity * 2;
+            data->lines = (char**)realloc(data->lines, newCapacity * sizeof(char*));
+            data->capacity = newCapacity;
+        }
+        int lineLength = strlen(buffer);
+        data->lines[data->lineCount] = (char*)malloc((lineLength + 1) * sizeof(char));
+        strcpy(data->lines[data->lineCount], buffer);
+        data->lineCount++;
     }
 }
 
-NumberRange* extract_number_ranges(const char **lines, int num_lines, int base, int *num_ranges) {
-    NumberRange *ranges = (NumberRange*) malloc(num_lines * sizeof(NumberRange));
-    *num_ranges = 0;
+// Funkcja wyodrębniająca ciągi znaków tworzące prawidłową liczbę dla podanego systemu liczbowego
+int* extractNumbers(DataStructure* data, int base) {
+    int* numbers = (int*)malloc(data->lineCount * sizeof(int));
+    for (int i = 0; i < data->lineCount; i++) {
+        numbers[i] = strtol(data->lines[i], NULL, base);
+    }
+    return numbers;
+}
 
-    for (int i = 0; i < num_lines; i++) {
-        const char *line = lines[i];
-        const int line_length = strlen(line);
+// Funkcja zwalniająca pamięć zajmowaną przez strukturę danych
+void freeDataStructure(DataStructure* data) {
+    for (int i = 0; i < data->lineCount; i++) {
+        free(data->lines[i]);
+    }
+    free(data->lines);
+    free(data);
+}
 
-        int j = 0;
-        while (j < line_length) {
-            // Pomijanie początkowych znaków, które nie tworzą liczby
-            while (j < line_length && !is_valid_digit(line[j], base)) {
-                j++;
-            }
+// Testy jednostkowe
 
-            if (j >= line_length) {
-                break;
-            }
-
-            // Znaleziono początek liczby
-            ranges[*num_ranges].start = &((char*)line)[j];
-
-            // Szukanie końca liczby
-            while (j < line_length && is_valid_digit(line[j], base)) {
-                j++;
-            }
-
-            // Znaleziono koniec liczby
-            ranges[*num_ranges].end = &((char*)line)[j-1];
-            (*num_ranges)++;
-
-            if (*num_ranges >= num_lines) {
-                // Dokonaj realokacji pamięci dla "niebieskiej" tablicy
-                ranges = (NumberRange*) realloc(ranges, (2 * num_lines) * sizeof(NumberRange));
-                num_lines *= 2;
-            }
-        }
+void runUnitTests() {
+    // Test initializeDataStructure
+    DataStructure* data = initializeDataStructure();
+    if (data->lineCount == 0 && data->capacity == INITIAL_CAPACITY) {
+        printf("Test initializeDataStructure: Passed\n");
+    } else {
+        printf("Test initializeDataStructure: Failed\n");
     }
 
-    return ranges;
+    // Test readLines
+    printf("Enter test lines (end with Ctrl+D):\n");
+    readLines(data);
+    if (data->lineCount > 0) {
+        printf("Test readLines: Passed\n");
+    } else {
+        printf("Test readLines: Failed\n");
+    }
+
+    // Test extractNumbers
+    int* numbers = extractNumbers(data, 10);
+    if (numbers != NULL) {
+        printf("Test extractNumbers: Passed\n");
+    } else {
+        printf("Test extractNumbers: Failed\n");
+    }
+    free(numbers);
+
+    // Test freeDataStructure
+    freeDataStructure(data);
+    printf("Test freeDataStructure: Passed\n");
+}
+
+// Test integracyjny
+
+void runIntegrationTest() {
+    printf("Enter test lines (end with Ctrl+D):\n");
+    DataStructure* data = initializeDataStructure();
+    readLines(data);
+    int* numbers = extractNumbers(data, 10);
+    printf("Extracted numbers:\n");
+    for (int i = 0; i < data->lineCount; i++) {
+        printf("%d\n", numbers[i]);
+    }
+    free(numbers);
+    freeDataStructure(data);
+}
+
+int main() {
+    DataStructure* data = initializeDataStructure();
+    printf("Enter lines (end with Ctrl+D):\n");
+    readLines(data);
+    int* numbers = extractNumbers(data, 10);
+    printf("Extracted numbers:\n");
+    for (int i = 0; i < data->lineCount; i++) {
+        printf("%d\n", numbers[i]);
+    }
+    free(numbers);
+    freeDataStructure(data);
+    printf("\nrun tests? (y/n): ");
+    char answer;
+    scanf("%c", &answer);
+    if (answer == 'y') {
+        runUnitTests();
+        runIntegrationTest();
+    }
+    
+
+    return 0;
 }
